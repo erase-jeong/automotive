12b83f6a7026731c12c4a69ea319a82d88e6edfe
Jiwoo Jeong <98355440+erase-jeong@users.noreply.github.com>
2024-04-24 07:23:33 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ st.color_picker('Choose your favorite color')
 
 
 st.sidebar.title("This is written inside the sidebar")
-st.sidebar.button("Click")
+st.sidebar.button("Clic")
 st.sidebar.radio("Pick your gender",["Male","Female"])
 
 
