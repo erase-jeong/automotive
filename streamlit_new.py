86bdc6ef015f448af94368a2593f5dedf73034d1
Jiwoo Jeong <98355440+erase-jeong@users.noreply.github.com>
2024-10-09 19:08:15 +0900

    feat : 포트설정추가

--- streamlit_new.py
+++ streamlit_new.py
@@ -1,7 +1,11 @@
+import os
 import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Heroku 포트 설정 추가
+port = os.environ.get("PORT", 8501)
 
 st.title('this is the app title')
 st.header("this is the markdown")
