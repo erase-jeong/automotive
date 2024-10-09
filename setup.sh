c4763e3d46696bdf9ffc8653428c88d84c876750
Jiwoo Jeong <98355440+erase-jeong@users.noreply.github.com>
2024-10-10 00:45:35 +0900

    .

--- setup.sh
+++ setup.sh
@@ -2,3 +2,8 @@ mkdir -p /app/.config/matplotlib
 export MPLCONFIGDIR=/app/.config/matplotlib
 rm -rf ~/.cache/matplotlib
 font_path='/app/fonts/MALGUN.TTF'
+
+# matplotlib에서 사용할 폰트 설정
+echo "font.family: sans-serif" >> /app/.config/matplotlib/matplotlibrc
+echo "font.sans-serif: Malgun Gothic" >> /app/.config/matplotlib/matplotlibrc
+echo "axes.unicode_minus: False" >> /app/.config/matplotlib/matplotlibrc
